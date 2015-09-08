170bcd3884744c7a173a0f5233bed449ee4a2543
Simon <simonthechain@gmail.com>
2015-09-08 00:16:57 -0400

    Update_21_OOP_2.0
    Created decks for each player

--- 21_OOP.py
+++ 21_OOP.py
@@ -1,5 +1,5 @@
 #module needed to randomize an integer
-from random import randrange
+from random import choice
 
 #asks to play again
 def credits():
@@ -17,10 +17,22 @@ def credits():
 
 #creates a class for the deck of cards
 class DeckOfCards(object):
-	def shuffle(self):
-		"""returns a randomized integer"""
-		shuffled = randrange(1, 11)
-		return shuffled
+	def __init__(self):
+		"""initializes a new deck with a list"""
+		self.cards = range(1, 11)
+
+	def reset(self):
+		"""resets the values of the decks"""
+		self.cards = range(1, 11)
+		return self.cards
+		
+	def deck_to_player(self, playerx_hand, playerx):
+		"""selects a value in the deck and transfers it to the player's hand"""
+		shuffle = choice(self.cards)
+		playerx_hand.append(shuffle)
+		playerx.hand += shuffle
+		self.deal = (self.cards).remove(shuffle)
+		return playerx.hand
 
 #creates a class for the players
 class Players(object):
@@ -29,9 +41,14 @@ class Players(object):
 		self.name = name
 		self.hand = hand
 	
-	def read_hand(self, player):
+	def reset_hand(self):
+		"""resets the values of the players' hands"""
+		self.hand = 0
+		return self.hand
+	
+	def read_hand(self, playerx):
 		"""displays the value of the player's hand"""
-		return player.name + str(player.hand) + " in hand."
+		return playerx.name + str(playerx.hand) + " in hand."
 	
 	def win_check1(self, code_exec_x):
 		"""checks if certain score conditions have been met"""
@@ -46,7 +63,8 @@ class Players(object):
 		return code_exec
 
 #creates objects from the classes
-deck = DeckOfCards()
+deck1 = DeckOfCards()
+deck2 = DeckOfCards()
 player1 = Players("\nYou have ", 0)
 player2 = Players("\nDealer has ", 0)
 
@@ -59,14 +77,26 @@ You'll start with two "cards"; try to reach 21 without going over!
 #sets the loop for the main program
 code_exec = [True, True, True, True]
 while code_exec[0] == True:
-
+	
+	#resets the values each round
+	deck1.reset()
+	deck2.reset()
+	player1.reset_hand()
+	player2.reset_hand()
+	player1_hand = []
+	player2_hand = []
+	
 	#list: [0]=program loop, [1]=player's turn, [2]=player2's turn, [3]=end results
 	code_exec = [True, True, True, True]
 
+	#bumper for each game
+	print "\n|----------|-new game-|----------|"
+	
 	#player's turn
-	player1.hand = deck.shuffle()
+	deck1.deck_to_player(player1_hand, player1)
 	while code_exec[1] == True:
-		player1.hand += deck.shuffle()
+		deck1.deck_to_player(player1_hand, player1)
+		print "\nYour hand:", player1_hand
 		print player1.read_hand(player1)
 		player1.win_check1(1)
 		if code_exec[1] == True:
@@ -83,11 +113,14 @@ while code_exec[0] == True:
 	#dealer's turn
 	if code_exec[2] == True:
 		print "\nDealer's turn:"
-		player2.hand = deck.shuffle() + deck.shuffle()
+		deck2.deck_to_player(player2_hand, player2)
+		deck2.deck_to_player(player2_hand, player2)
+		print "\nDealer's hand:", player2_hand
 		print player2.read_hand(player2)
 		while player2.hand < 17:
 			raw_input("Press Enter.")
-			player2.hand += deck.shuffle()
+			deck2.deck_to_player(player2_hand, player2)
+			print "\nDealer's hand:", player2_hand
 			print player2.read_hand(player2)
 			player2.win_check1(2)
 
